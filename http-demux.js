1114fad9a670587ff1ffc1803d54c328f4ef6101
Nathan Rajlich <nathan@tootallnate.net>
2018-01-17 19:35:16 -0800

    Add "args" CLI

--- http-demux.js
+++ http-demux.js
@@ -1,20 +1,35 @@
 #!/usr/bin/env node
 /**
- * TCP server that forwards connections to an `http-mux` server.
- * HTTP framing is added around the connection.
+ * TCP server that forwards connections to an `http-mux` WebSocket server.
+ * WebSocket framing is added around the connection.
  */
-
 const net = require('net');
+const url = require('url');
+const args = require('args');
 const listen = require('async-listen');
 const debug = require('debug')('http-demux');
 const websocket = require('websocket-stream');
 const { debugStream } = require('./util');
 
-async function main() {
+args
+  .option('target', 'The target `http-mux` server URL to connect to', 'localhost:3000')
+  .option('port', 'The port on which the TCP server will be running', 3001);
+
+const flags = args.parse(process.argv)
+
+async function main({ target, port }) {
+  let parsed = url.parse(target);
+  if (!parsed.slashes) {
+    parsed = url.parse(`http://${target}`);
+  }
+  parsed.protocol = /s\:$/.test(parsed.protocol) ? 'wss:' : 'ws:';
+  const targetWs = url.format(parsed);
+  debug('Forwarding connections to %o', targetWs);
+
   const server = net.createServer(socket => {
     debug('Socket connection');
     socket.on('error', err => debug('socket error %o', err));
-    const ws = websocket('ws://localhost:3000');
+    const ws = websocket(targetWs);
     ws.on('error', err => debug('ws error %o', err));
     debug('WebSocket "open" event');
     socket.pipe(ws);
@@ -22,11 +37,11 @@ async function main() {
     debugStream(debug, 'ws', ws);
     debugStream(debug, 'socket', socket);
   });
-  const addr = await listen(server, 3001);
+  const addr = await listen(server, port);
   console.log(addr);
 }
 
-main().catch(err => {
+main(flags).catch(err => {
   console.error(err);
   process.exit(1);
 });
--- http-mux.js
+++ http-mux.js
@@ -1,53 +1,51 @@
 #!/usr/bin/env node
 /**
- * HTTP server that forwards HTTP requests to a TCP server of some kind.
- * The HTTP framing is stripped away so that the request body only is
- * sent to the TCP server.
+ * WebSocket server that forwards connections to an upstream TCP server.
+ * The WebSocket framing is stripped away so that the message bodes only
+ * are sent to the TCP server.
  */
-
 const net = require('net');
+const args = require('args');
 const http = require('http');
 const listen = require('async-listen');
 const debug = require('debug')('http-mux');
 const websocket = require('websocket-stream');
 const { debugStream } = require('./util');
 
-async function main() {
+args
+  .option('upstream-host', 'The upstream host to connect to', 'localhost')
+  .option('upstream-port', 'The upstream port to connect to', 6379)
+  .option('port', 'The port on which the WebSocket server will be running', 3000);
+
+const flags = args.parse(process.argv)
+
+async function main({ upstreamHost, upstreamPort, port }) {
+  debug('Forwarding connections to %o', `${upstreamHost}:${upstreamPort}`);
+
   const server = http.createServer((req, res) => {
-    /*
-    req.on('error', err => debug('req error %o', err));
-    res.on('error', err => debug('res error %o', err));
-    if (req.method == 'POST') {
-      const socket = net.connect({ port: 6379 });
-      socket.once('connect', () => debug('socket connected'));
-      socket.on('error', err => debug('socket error %o', err));
-      res.writeHead(200);
-      debugStream(debug, 'req', req);
-      req.pipe(socket);
-      debugStream(debug, 'socket', socket);
-      socket.pipe(res);
-    }
-    */
     res.statusCode = 404;
     res.end();
   });
-  const wss = websocket.createServer({
-    //perMessageDeflate: false,
-    server
-  }, ws => {
-    const socket = net.connect({ port: 6379 });
-    socket.once('connect', () => debug('socket connected'));
-    socket.on('error', err => debug('socket error %o', err));
-    socket.pipe(ws);
-    ws.pipe(socket);
-    debugStream(debug, 'ws', ws);
-    debugStream(debug, 'socket', socket);
-  })
-  const addr = await listen(server, 3000);
-  console.log(addr);
+  const wss = websocket.createServer(
+    {
+      perMessageDeflate: false,
+      server
+    },
+    ws => {
+      const socket = net.connect({ host: upstreamHost, port: upstreamPort });
+      socket.once('connect', () => debug('socket connected'));
+      socket.on('error', err => debug('socket error %o', err));
+      socket.pipe(ws);
+      ws.pipe(socket);
+      debugStream(debug, 'ws', ws);
+      debugStream(debug, 'socket', socket);
+    }
+  );
+  const addr = await listen(server, port);
+  console.log('http-mux WebSocket server listening at %s', addr);
 }
 
-main().catch(err => {
+main(flags).catch(err => {
   console.error(err);
   process.exit(1);
 });
